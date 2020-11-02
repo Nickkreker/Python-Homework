12a4f2aeb3d59d83eb85c40d6595f624dae189c8
Nickkreker <nick_berezikov@me.com>
2020-11-02 14:53:11 +0300

    task3 done

---
+++ todo/models.py
@@ -0,0 +1,80 @@
+class BaseItem(object):
+    def __init__(self, heading):
+        self.heading = heading
+        self.done = False  # TODO: make sure we can use it...
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+    @classmethod
+    def construct(cls):
+        raise NotImplementedError()
+
+
+class ToDoItem(BaseItem):
+    def __str__(self):
+        status=''
+        if self.done:
+            status = '+'
+        else:
+            status = '-'
+
+        return '{0} ToDoItem: {1}'.format(
+            status,
+            self.heading,
+        )
+
+    @classmethod
+    def construct(cls):
+        heading = input('Input heading: ')
+        return cls(heading)
+
+
+class ToBuyItem(BaseItem):
+    def __init__(self, heading, price):
+        super().__init__(heading)
+        self.price = price
+
+    def __str__(self):
+        status = ''
+        if self.done:
+            status = '+'
+        else:
+            status = '-'
+
+        return '{0} ToBuyItem: {1} for {2}'.format(
+            status,
+            self.heading,
+            self.price,
+        )
+
+    @classmethod
+    def construct(cls):
+        heading = input('Input heading: ')
+        price = input('Input price: ')
+        return cls(heading, price)
+
+
+class ToReadItem(BaseItem):
+    def __init__(self, heading, url):
+        super().__init__(heading)
+        self.url = url
+
+    def __str__(self):
+        status = ''
+        if self.done:
+            status = '+'
+        else:
+            status = '-'
+
+        return '{0} ToReadItem: {1} on {2}'.format(
+            status,
+            self.heading,
+            self.url,
+        )
+
+    @classmethod
+    def construct(cls):
+        heading = input('Input heading: ')
+        url = input('Input url: ')
+        return cls(heading, url)
